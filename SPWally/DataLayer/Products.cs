06443208b453510a3ef1dd4f95253a31ba8762d1
Stephen Perrin <Sperrin7009@conestogac.on.ca>
2019-12-01 17:06:57 -0500

    Added Products Data Class

---
+++ SPWally/DataLayer/Products.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPWally.DataLayer
+{
+    class Products
+    {
+        public int productId { get; set; }
+        public string ProductName { get; set; }
+        public double wPrice {
+            get { return wPrice; }
+
+            set
+            {
+                if(value > 0.00)
+                {
+                    wPrice = value;
+                }
+            }
+        }
+        public int Stock
+        {
+            get { return Stock; }
+
+            set
+            {
+                if(value >= 0)
+                {
+                    Stock = value;
+                }
+            }
+        }
+    }
+}
