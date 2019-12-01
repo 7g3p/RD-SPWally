346f515b119b2742830e8d92d82f47dd1ffcb47f
sperrin7009 <sperrin7009@2A213-C04>
2019-11-30 22:17:55 -0500

    check the picture for more details on the changes. Place order now uses a listbox for branches

--- SPWally/FunctionalPages/ExistingCustomer.xaml.cs
+++ SPWally/FunctionalPages/ExistingCustomer.xaml.cs
@@ -24,5 +24,26 @@ namespace SPWally.FunctionalPages
         {
             InitializeComponent();
         }
+
+        private void ConfirmCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            // Find the frame.
+            Frame frame = null;
+            DependencyObject parent = VisualTreeHelper.GetParent(this);
+
+            // Cycles through to MainWindow frame
+            while (parent != null && frame == null)
+            {
+                frame = parent as Frame;
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            // Change the page of the frame.
+            if (frame != null)
+            {
+                frame.Navigate(new RefundOrder());
+            }
+            //Code Courtesy of Shmuel Zang in codeprojects.com https://www.codeproject.com/Questions/281551/frame-navigation-in-WPF
+        }
     }
 }
